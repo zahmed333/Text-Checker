a85e3b00f09a4cd742e92a4e3dfc14a2acc64348
thanhdang2712 <danghuyenthanh2003@gmail.com>
2023-11-04 02:10:30 -0400

    Handle selected text on screen

--- background.js
+++ background.js
@@ -1,4 +1,38 @@
 // background.js
+// Background script to handle the selected text: this should replace the ar query = request.query; line of code
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'selectedText') {
+    // Process the selected text and prepare results (subject, summary, sources, errors)
+    const results = processSelectedText(message.selectedText);
+
+    // Send the results to the popup script
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0];
+      chrome.scripting.executeScript({
+        target: { tabId: activeTab.id },
+        function: sendResultsToPopup,
+        args: [results],
+      });
+    });
+  }
+});
+
+// Function to process selected text (we need to implement this)
+function processSelectedText(selectedText) {
+  // Implement the text processing logic here
+  const subject = 'Your Subject'; // Example subject
+  const summary = 'Summary of the selected text'; // Example summary
+  const sources = ['Source 1', 'Source 2']; // Example sources
+  const errors = []; // Example errors
+
+  return { subject, summary, sources, errors };
+}
+
+// Function to send the results to the popup script
+function sendResultsToPopup(results) {
+  chrome.runtime.sendMessage({ type: 'updatePopupUI', results });
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     var query = request.query;
    
